2e7d1e2f42bb23db269ace86e2f8aa3acb3a2acf
pitisalcedo <66760644+pitisalcedo@users.noreply.github.com>
2020-09-15 22:51:48 -0500

    Updated Footer.js
    Added target to anchor element

--- src/Footer.js
+++ src/Footer.js
@@ -5,14 +5,17 @@ export default function Footer() {
   return (
     <div className="open-source-code">
       This project was built by
-      <a href="https://www.linkedin.com/in/priscillasalcedo/">
+      <a href="https://www.linkedin.com/in/priscillasalcedo/" target="_blank">
         {" "}
         {""}
         Priscilla Salcedo
       </a>{" "}
       {""}
       and open-sourced on
-      <a href="https://github.com/pitisalcedo/weather-react"> {""} Github</a>
+      <a href="https://github.com/pitisalcedo/weather-react" target="_blank">
+        {" "}
+        {""} Github
+      </a>
     </div>
   );
 }
